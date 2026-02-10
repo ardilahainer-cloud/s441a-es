baf13cfedcf2799f0a7942fe57e0cb4566021eea
ardilahainer-cloud <ardilahainer@gmail.com>
2026-02-10 16:57:49 +0000

    se agrego una suma de numeros pares al arreglo

--- semana2/Ejercicio1.java
+++ semana2/Ejercicio1.java
@@ -4,9 +4,18 @@ public class Ejercicio1 {
     public static void main (String [] args){
        //Forma1 -> Definiendo directamente los valores
         int[] a ={4,8,9,6,1,2,10};
+        int pares = 0;
+        int impares=0;
         for (int i = 0; i < a.length;i++){
-            System.out.println("a["+i+"]="+a[i]);
-        }
+            if (a[i]%2==0){
+                pares +=a[i];
+            }else{
+                    impares +=a[i];
+                }
+            }
+           
+            System.out.println("los nu8meros pares son"+pares);
+           System.out.println ("los numeros impares son"+impares);
     }
     
 }
