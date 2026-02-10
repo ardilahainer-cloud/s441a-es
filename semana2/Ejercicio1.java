648d73048ebdcb497168d345eff2afd06eb21725
ardilahainer-cloud <ardilahainer@gmail.com>
2026-02-10 16:27:59 +0000

    primer ejercicio con arreglos

---
+++ semana2/Ejercicio1.java
@@ -0,0 +1,13 @@
+package semana2;
+
+public class Ejercicio1 {
+    public static void main (String [] args){
+       //Forma1 -> Definiendo directamente los valores
+        int[] a ={4,8,9,6,1,2};
+        for (int i = 0; i < a.length;i++){
+            System.out.println("a["+i+"]="+a[i]);
+        }
+    }
+    
+}
+
