2a67268e4b5ab68fb0d2054e86cf79811875ed1c
ardilahainer-cloud <ardilahainer@gmail.com>
2026-02-10 16:30:45 +0000

    cambio

--- semana2/Ejercicio1.java
+++ semana2/Ejercicio1.java
@@ -3,7 +3,7 @@ package semana2;
 public class Ejercicio1 {
     public static void main (String [] args){
        //Forma1 -> Definiendo directamente los valores
-        int[] a ={4,8,9,6,1,2};
+        int[] a ={4,8,9,6,1,2,10};
         for (int i = 0; i < a.length;i++){
             System.out.println("a["+i+"]="+a[i]);
         }
