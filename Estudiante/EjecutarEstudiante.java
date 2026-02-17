10ccf0adc49c7fae19ca06fceed4dfe42d2201ee
ardilahainer-cloud <ardilahainer@gmail.com>
2026-02-17 17:36:28 +0000

    Arreglo de objetos

---
+++ Estudiante/EjecutarEstudiante.java
@@ -0,0 +1,46 @@
+package Estudiante;
+
+public class EjecutarEstudiante {
+    public static void main(String[] args) {
+        Estudiante objE = new Estudiante();
+        Estudiante[]est=new Estudiante[4];
+        est[0]= new Estudiante("luis",3,"Industrial",3.4,34);
+        est[1]= new Estudiante("dory",2,"Sistema ",3.9,18);
+        est[2]= new Estudiante("jaime",5,"Trabajo social",3.5,12);
+        est[3]= new Estudiante("adrian",3,"Industrial ",2.5,25);
+        
+        System.out.println(est[2]);
+        System.out.println(est[0].getNombre()+ "   " +est[0].getPrograma());
+        
+        for (int i=0; i < est.length;i++){
+            System.out.println(est[i].getNombre() + "---->"+ est[i].getPrograma());
+        }
+
+        double sumaNota=0.0;
+        for (int i=0; i < est.length;i++){
+          sumaNota +=  est[i].getNotaPromedio();
+
+        }
+        System.out.println(sumaNota / est.length);
+
+        int[]edades = new int[est.length];
+       
+        for (int i = 0; i < edades.length; i++) {
+            edades[i] = est[i].getEdad();
+        }
+ 
+        //Uso del método calcularEdadPromedio
+        System.out.println(objE.calcularEdadPromedio(edades));
+
+        for (int i=0; i < est.length;i++){
+            System.out.println(est[i].getNotaPromedio() + "------->"+ est[i].getSemestre()+"------>"+est[i].getPrograma());
+
+
+        }
+
+        
+
+    
+    }
+    
+}
--- semana2/Ejercicio1.java
+++ semana2/Ejercicio1.java
@@ -4,6 +4,11 @@ public class Ejercicio1 {
     public static void main (String [] args){
        //Forma1 -> Definiendo directamente los valores
         int[] a ={4,8,9,6,1,2,10};
+      
+        for (int i = 0; i < a.length;i++){
+        System.out.println("a["+i+"]="+a[1]);
+        }
+        
         int pares = 0;
         int impares=0;
         for (int i = 0; i < a.length;i++){
@@ -12,10 +17,10 @@ public class Ejercicio1 {
             }else{
                     impares +=a[i];
                 }
-            }
+        }
            
-            System.out.println("los nu8meros pares son"+pares);
-           System.out.println ("los numeros impares son"+impares);
+            System.out.println("los numeros pares son"+pares);
+            System.out.println ("los numeros impares son"+impares);
     }
     
 }
